2cef2dbe8998c58f108f630596201bd411379032
aforarsh <82162012+aforarsh@users.noreply.github.com>
2021-06-19 21:45:43 +0800

    commit

--- ecommerce/PlaceOrder.aspx.cs
+++ ecommerce/PlaceOrder.aspx.cs
@@ -15,22 +15,5 @@ namespace ecommerce
         {
 
         }
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-20VP0PUP\SQLEXPRESS;Initial Catalog=ecommerce;Integrated Security=True;");
-            if (FileUpload1.HasFile)
-            {
-                string filename = FileUpload1.PostedFile.FileName;
-                string filepath = "receipts/" + FileUpload1.FileName;
-                FileUpload1.PostedFile.SaveAs(Server.MapPath("~/receipts/") + filename);
-                con.Open();
-                SqlCommand cmd = new SqlCommand("UPDATE tb_Order SET Payment_Prove=@file WHERE Order_ID=" + Request.QueryString["oid"], con);
-                cmd.Parameters.AddWithValue("@file", filepath);
-                cmd.ExecuteNonQuery();
-                con.Close();
-                Response.Redirect("Default.aspx");
-            }
-        }
     }
 }
