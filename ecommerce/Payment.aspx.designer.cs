368dd6e334a01410c83b5f28c628a439f22a4b40
aforarsh <82162012+aforarsh@users.noreply.github.com>
2021-06-20 14:41:55 +0800

    designer

--- ecommerce/Payment.aspx.designer.cs
+++ ecommerce/Payment.aspx.designer.cs
@@ -23,14 +23,6 @@ namespace ecommerce
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// FileUpload1 control.
